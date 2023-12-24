54d4852b2de43ee15c5d7fe6fbb359453b2b2b16
Akash Mondal <a98mondal@gmail.com>
2023-12-25 01:59:50 +0530

    gemi: keep micro awake for n secs
    * create a API logic to keep micro awake

--- api/app.py
+++ api/app.py
@@ -1,5 +1,8 @@
+from asyncio import sleep
 from typing import Any, Dict, Annotated, Union
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
+from aiohttp import ClientSession
+from os import getenv
 
 from chat.service import ChatService
 from containers import BotContainer
@@ -17,3 +20,17 @@ async def root(chat_service: Annotated[ChatService, Depends(lambda: BotContainer
 @app.get("/ping")
 async def ping():
     return {"message": "pong"}
+
+@app.get("/keepawake")
+async def ping(secs: int = 20):
+    # keep awake for n sec
+    await sleep(secs)
+    return {"message": "awake for " + str(secs) + " secs"}
+
+@app.post("/__space/v0/actions")
+async def actions(req: Request):
+    body = await req.json()
+    if(body['event']['id'] == 'wakeup'):
+        async with ClientSession(conn_timeout=20) as session:
+            async with session.get(req.base_url.scheme + '://' + getenv('DETA_SPACE_APP_HOSTNAME') + '/keepawake?secs=50') as res:
+                return await res.text()
--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import asyncio
 import logging
 import sys
+from os import getenv
 from os.path import exists
+import uvicorn
 
 from containers import BotContainer, Configs
-from api.app import app as api
+from api.app import app
+
+api_reload = False
 
 if exists(".env"):
     from dotenv import load_dotenv
     # Load .env file if it exists
     load_dotenv(".env")
+    api_reload = True
     logging.info("Loaded local .env file")
 
-@api.on_event("startup")
+@app.on_event("startup")
 async def on_server_startup():
     configs = Configs
 
     # API key can be obtained via https://platform.openai.com/account/api-keys
     configs.chat_config.api_key.from_env("GOOGLE_API_KEY", required=True)
 
+async def run_server():
+    config = uvicorn.Config(app=app, host="0.0.0.0", port=int(getenv("PORT", 8000)), reload=api_reload)
+    server = uvicorn.Server(config)
+    await server.serve()
+
 async def main():
+    logging.info("Starting API...")
+    asyncio.create_task(run_server())
+
     logging.info("Starting bot...")
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = BotContainer.tg_bot()
