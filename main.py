b02fa7e75a48b7923f39b3bd61aea8aa13d87eac
Akash Mondal <a98mondal@gmail.com>
2024-01-11 20:15:35 +0530

    gemi (metrices): reduce profiling smaple rate

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def log_integration():
             # Set profiles_sample_rate to 1.0 to profile 100%
             # of sampled transactions.
             # We recommend adjusting this value in production.
-            profiles_sample_rate=1.0,
+            profiles_sample_rate=0.3,
             environment=getenv('ENV', 'dev'),
             integrations=[
                 LoggingIntegration(
