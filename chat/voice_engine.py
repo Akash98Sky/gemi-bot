c9e01e9d96a12cef5097d7308ba034f6506f870f
Akash Mondal <a98mondal@gmail.com>
2024-02-13 01:56:10 +0530

    gemi (voice): no need to lock semaphore for each request
    * cancel engine up task -> close client

--- chat/voice_engine.py
+++ chat/voice_engine.py
@@ -15,6 +15,7 @@ class VoiceEngine:
     __stt: str
     __voice: str
     __voice_api_url: str
+    __engine_up_task: asyncio.Task
     
     def __init__(self, voice_api_url: str | None, tts_voice: str, stt_engine: str):
         if not voice_api_url or not voice_api_url.strip():
@@ -23,7 +24,7 @@ class VoiceEngine:
         self.__stt = stt_engine
         self.__voice_api_url = voice_api_url
         self.__client_session = ClientSession(base_url=voice_api_url)
-        asyncio.create_task(self.__bring_up_engine__())
+        self.__engine_up_task = asyncio.create_task(self.__bring_up_engine__())
         loop = asyncio.get_running_loop()
         with suppress(NotImplementedError):  # pragma: no cover
             # Signals handling is not supported on Windows
@@ -50,31 +51,40 @@ class VoiceEngine:
         logging.info("Voice engine is up...")
 
     def __close_client(self):
+        if not self.__engine_up_task.done():
+            self.__engine_up_task.cancel()
         if self.__client_session:
             asyncio.create_task(self.__client_session.close())
             self.__client_session = None
+        logging.info("Voice engine is closed...")
 
     async def text_to_wave(self, text: str):
         if not self.__client_session:
             raise UnsupportedException("Voice engine is not enabled.")
         
         async with self.__engine_busy_sem:
-            data = JsonPayload({'text': text, 'voice_id': self.__voice})
-            async with self.__client_session.post(f'/api/speak/{self.__tts}', data=data) as response:
-                if response.status == 200:
-                    res = await response.json()
-                    return str(res['url'])
-                logging.error(f"Voice engine returned error status: {response.status}")
+            # Wait until engine is up
+            pass
+
+        data = JsonPayload({'text': text, 'voice_id': self.__voice})
+        async with self.__client_session.post(f'/api/speak/{self.__tts}', data=data) as response:
+            if response.status == 200:
+                res = await response.json()
+                return str(res['url'])
+            logging.error(f"Voice engine returned error status: {response.status}")
 
     async def voice_to_text(self, voice: bytes, content_type: str = 'audio/wav'):
         if not self.__client_session:
             raise UnsupportedException("Voice engine is not enabled.")
         
         async with self.__engine_busy_sem:
-            data = FormData()
-            data.add_field('file', voice, content_type=content_type)
-            async with self.__client_session.post(f'/api/listen/{self.__stt}', data=data) as response:
-                if response.status == 200:
-                    res = await response.json()
-                    return str(res['text'])
-                logging.error(f"Voice engine returned error status: {response.status}")
+            # Wait until engine is up
+            pass
+
+        data = FormData()
+        data.add_field('file', voice, content_type=content_type)
+        async with self.__client_session.post(f'/api/listen/{self.__stt}', data=data) as response:
+            if response.status == 200:
+                res = await response.json()
+                return str(res['text'])
+            logging.error(f"Voice engine returned error status: {response.status}")
